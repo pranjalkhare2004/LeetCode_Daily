595b9f584c9fe27322e061bdf1a63882599ac784
Pranjal Khare <112844315+pranjalkhare2004@users.noreply.github.com>
2026-02-23 00:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (70.75%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+
+        /*
+Given: +ve integer n
+1. find
+2. return longest dist b/w 2 adjacent 1's in representation of n
+3. if no adjacent 1's return 0
+
+eg: 1001 -> 3-0 =  3
+
+100101 -> 5-2 = 3 -> max(3, 2)
+
+find set bit using left shift
+
+approach:
+
+find set bit
+n & 1<<
+set bit 1 = 
+set bit 2 = 
+dist = set bit 2 -  set bit 1
+maxdist = max(maxdist, dist)
+
+*/
+        int curr = 0;
+        int prev = -1;
+        int result = 0;
+
+        while(n > 0){
+            if((n&1) > 0){
+                result = (prev != -1) ? max(result, curr-prev) : result;
+                prev = curr;
+            }
+            n>>=1;
+            curr++;
+        }
+        return result;
+    }
+};
