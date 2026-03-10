68a7a958b5f081ae248ab19d9e1a77dbd734751c
Pranjal Khare <112844315+pranjalkhare2004@users.noreply.github.com>
2026-03-10 20:10:45 +0530

    Time: 1660 ms (23.29%) | Memory: 551.1 MB (24.66%) - LeetSync

--- 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.cpp
+++ 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
+    constexpr static int MOD = 1000000007;
     int numberOfStableArrays(int zero, int one, int limit) {
-        int mod = 1e9 + 7;
-        vector<vector<vector<int>>> memo(
-            zero + 1, vector<vector<int>>(one + 1, vector<int>(2, -1)));
-
-        function<int(int, int, int)> dp = [&](int zero, int one,
-                                              int lastBit) -> int {
-            if (zero == 0) {
-                return (lastBit == 0 || one > limit) ? 0 : 1;
-            } else if (one == 0) {
-                return (lastBit == 1 || zero > limit) ? 0 : 1;
-            }
-
-            if (memo[zero][one][lastBit] == -1) {
-                int res = 0;
-                if (lastBit == 0) {
-                    res = (dp(zero - 1, one, 0) + dp(zero - 1, one, 1)) % mod;
-                    if (zero > limit) {
-                        res = (res - dp(zero - limit - 1, one, 1) + mod) % mod;
+        vector<vector<vector<int>>> dp(
+            zero + 1, vector<vector<int>>(one + 1, vector<int>(2)));
+        for (int i = 0; i <= zero; i++) {
+            for (int j = 0; j <= one; j++) {
+                for (int lastBit = 0; lastBit <= 1; lastBit++) {
+                    if (i == 0) {
+                        if (lastBit == 0 || j > limit) {
+                            dp[i][j][lastBit] = 0;
+                        } else {
+                            dp[i][j][lastBit] = 1;
+                        }
+                    } else if (j == 0) {
+                        if (lastBit == 1 || i > limit) {
+                            dp[i][j][lastBit] = 0;
+                        } else {
+                            dp[i][j][lastBit] = 1;
+                        }
+                    } else if (lastBit == 0) {
+                        dp[i][j][lastBit] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                        if (i > limit) {
+                            dp[i][j][lastBit] -= dp[i - limit - 1][j][1];
+                        }
+                    } else {
+                        dp[i][j][lastBit] = dp[i][j - 1][0] + dp[i][j - 1][1];
+                        if (j > limit) {
+                            dp[i][j][lastBit] -= dp[i][j - limit - 1][0];
+                        }
                     }
-                } else {
-                    res = (dp(zero, one - 1, 0) + dp(zero, one - 1, 1)) % mod;
-                    if (one > limit) {
-                        res = (res - dp(zero, one - limit - 1, 0) + mod) % mod;
+                    dp[i][j][lastBit] %= MOD;
+                    if (dp[i][j][lastBit] < 0) {
+                        dp[i][j][lastBit] += MOD;
                     }
                 }
-                memo[zero][one][lastBit] = res % mod;
             }
-            return memo[zero][one][lastBit];
-        };
+        }
 
-        return (dp(zero, one, 0) + dp(zero, one, 1)) % mod;
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 };
